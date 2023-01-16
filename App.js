af1e6e1c371a02ef32708c72bdf464a6907fc97a
RyoMurakami <ryoyuki6@yahoo.co.jp>
2023-01-16 18:28:33 +0900

    profile edit screen

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { NativeBaseProvider, Text, Box, VStack, Stack } from "native-base";
-
-import { Button } from "react-native";
+import {
+  NativeBaseProvider,
+  Text,
+  Box,
+  VStack,
+  Stack,
+  Button
+} from "native-base";
 
 import { API, Amplify, graphqlOperation } from "aws-amplify";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
@@ -15,14 +20,16 @@ import { useNavigation } from "@react-navigation/native";
 import awsExports from "./src/aws-exports";
 Amplify.configure(awsExports);
 
+import { ProfileEditScreen } from './src/screens/ProfileEditScreen';
+
 function SignOutButton() {
   const { signOut } = useAuthenticator();
-  return <Button title="Sign Out" onPress={signOut} />;
+  return <Button onPress={signOut}>Sign Out</Button>;
 }
 
 function HomeScreen() {
   const navigation = useNavigation();
-  const { user, signOut } = useAuthenticator((context) => [context.user])
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   return (
     <NativeBaseProvider>
@@ -40,10 +47,7 @@ function HomeScreen() {
           <Text fontSize="sm" color="white">
             {`Welcome, ${user.username} !!!`}
           </Text>
-          <Button
-            title="Profileへ"
-            onPress={() => navigation.navigate("Profile")}
-          />
+          <Button onPress={() => navigation.navigate("Profile")}>Profileへ</Button>
           <SignOutButton />
         </VStack>
       </Box>
@@ -53,7 +57,8 @@ function HomeScreen() {
 
 function ProfileScreen() {
   const [data1, setData1] = useState([]);
-  const { user, signOut } = useAuthenticator((context) => [context.user])
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  const navigation = useNavigation();
 
   useEffect(() => {
     fetchData1();
@@ -61,9 +66,9 @@ function ProfileScreen() {
 
   async function fetchData1() {
     const opt = {
-      filter: {name: {eq: user.username}},
+      filter: { name: { eq: user.username } },
     };
-    API.graphql(graphqlOperation(listPeople,opt)).then(values=> {
+    API.graphql(graphqlOperation(listPeople, opt)).then((values) => {
       setData1(values.data.listPeople.items);
     });
   }
@@ -86,6 +91,7 @@ function ProfileScreen() {
           <Text fontSize="sm" color="white">
             Tel : {data1[0].tel}
           </Text>
+          <Button onPress={() => navigation.navigate("ProfileEdit")}>Profile編集画面へ</Button>
         </>
       );
     }
@@ -113,6 +119,133 @@ function ProfileScreen() {
   );
 }
 
+// function ProfileEditScreen () {
+//   const [ text, setText ] = useState('');
+
+//   const onPressSave = () => {
+//       alert(text);
+//   }
+
+//   return (
+//       <KeyboardAvoidingView
+//           style={styles.container}
+//       >
+//           <TextInput 
+//               style={{ marginBottom: 16 }}
+//               mode="outlined"
+//               placeholder="メモを入力してください！！！"
+//               multiline
+//               onChangeText={(text) => setText(text)}
+//           />
+//           <Button
+//               mode="contained"
+//               onPress={onPressSave}
+//           >
+//               保存！！！
+//           </Button>
+//       </KeyboardAvoidingView>
+//   )
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//       flex: 1,
+//       padding: 16,
+//   },
+// });
+
+// function ProfileEditScreen() {
+
+//   return (
+//     <NativeBaseProvider>
+//       <Box
+//         bg="primary.500"
+//         py="4"
+//         px="3"
+//         borderRadius="5"
+//         rounded="md"
+//         alignSelf="center"
+//         justifyContent="center"
+//       >
+//         <VStack space="2" alignSelf="center">
+//           <Text fontSize="sm" color="white">
+//             Profile2
+//           </Text>
+//           {/* <DisplayData0 /> */}
+//         </VStack>
+//       </Box>
+//     </NativeBaseProvider>
+//   );
+// }
+
+// function ProfileEditScreen() {
+  // const [data1, setData1] = useState([]);
+  // const { user, signOut } = useAuthenticator((context) => [context.user]);
+  // const [text, setText] = useState("入力文字が表示されます");
+
+  // useEffect(() => {
+  //   fetchData1();
+  // }, []);
+
+  // async function fetchData1() {
+  //   const opt = {
+  //     filter: { name: { eq: user.username } },
+  //   };
+  //   API.graphql(graphqlOperation(listPeople, opt)).then((values) => {
+  //     setData1(values.data.listPeople.items);
+  //   });
+  // }
+
+  // console.log(data1);
+
+  // const onPressSave = () => {
+  //   console.log(text);
+  // };
+
+  // function DisplayData0() {
+  //   if (typeof data1[0] !== "undefined") {
+  //     return (
+  //       <>
+  //         <Text fontSize="sm" color="white">
+  //           Email, {text}
+  //         </Text>
+  //         <TextInput
+  //           style={{ marginBottom: 16}}
+  //           mode="outlined"
+  //           placeholder={data1[0].email}
+  //           multiline
+  //           // onChangeText={t => setText(t)}
+  //           onChangeText={setText}
+  //         />
+  //         <Button onPress={onPressSave} mode="contained">save</Button>
+  //       </>
+  //     );
+  //   }
+  // }
+
+  // return (
+  //   <NativeBaseProvider>
+  //     <Box
+  //       bg="primary.500"
+  //       py="4"
+  //       px="3"
+  //       borderRadius="5"
+  //       rounded="md"
+  //       alignSelf="center"
+  //       justifyContent="center"
+  //       w={300}
+  //     >
+  //       <VStack space="2" alignSelf="center">
+  //         <Text fontSize="sm" color="white">
+  //           Profile
+  //         </Text>
+  //         <DisplayData0 />
+  //       </VStack>
+  //     </Box>
+  //   </NativeBaseProvider>
+  // );
+// }
+
 const Stack2 = createStackNavigator();
 
 function App() {
@@ -123,6 +256,7 @@ function App() {
           <Stack2.Navigator initialRouteName="Home">
             <Stack2.Screen name="Home" component={HomeScreen} />
             <Stack2.Screen name="Profile" component={ProfileScreen} />
+            <Stack2.Screen name="ProfileEdit" component={ProfileEditScreen} />
           </Stack2.Navigator>
         </NavigationContainer>
       </Authenticator>
